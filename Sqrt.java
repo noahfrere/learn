64bdb322e83281a1f693e62bd36e164b22d69f20
Noah Frere <nfrere@vols.utk.edu>
2016-01-23 22:19:39 -0500

    Create Sqrt.java

---
+++ Sqrt.java
@@ -0,0 +1,13 @@
+package jayJay;
+
+public class Sqrt {
+
+	public static void main(String[] args) {
+
+		
+		SqrtFunction Newton = new SqrtFunction();
+		Newton.get(number);
+		System.out.println(number);
+	
+	}
+}
