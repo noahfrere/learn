8e47cae882036d93a1ec148e207237627f4a265b
Noah Frere <nfrere@vols.utk.edu>
2016-01-23 23:00:25 -0500

    Update Sqrt.java
    I tried here to put everything except for Newton's Method itself into main(). The error I keep getting is on line 13 and 15, which says "number cannot be resolved to a variable" and "x cannot be resolved to a variable."

--- Sqrt.java
+++ Sqrt.java
@@ -1,13 +1,28 @@
 package jayJay;
 
+import java.util.Scanner;
+
 public class Sqrt {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
 		
 		SqrtFunction Newton = new SqrtFunction();
 		Newton.get(number);
+		
+		double number = x;
+		
+		double x = 0;
+		double x1, x2, x3;
+		double S;
+		
+		System.out.print("Wilkommen to the Square Root function per Newton. Please enter a number "
+				+ "that you want the square root of.");
+		S = sc.nextDouble();
+		System.out.print("Now enter your best guess for the answer.");
+		x = sc.nextDouble();
 		System.out.println(number);
 	
 	}
-}
